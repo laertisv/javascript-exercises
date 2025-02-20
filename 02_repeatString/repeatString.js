1876fc4f2ab71950b076fc5cf6c995c5142c1179
Laertis Vaso <laertis.vaso@gmail.com>
2025-02-20 19:03:39 +0100

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    finalText = '';
+    if (times < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < times; i++) {
+        finalText += word;
+    }
+    return finalText;
 };
 
 // Do not edit below this line
