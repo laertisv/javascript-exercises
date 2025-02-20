0210cc430b00f716a5680399fd5f85c7bb009725
Laertis Vaso <laertis.vaso@gmail.com>
2025-02-20 19:28:00 +0100

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    else if (a % 1 !== 0 || b % 1 !== 0) {
+        return 'ERROR';
+    }
 
+    if (a < b) {
+        start = a; 
+        finish = b;
+    }
+    else {
+        start = b;
+        finish = a;
+    }
+    
+    sum = 0;
+    for (start; start <= finish; start++) {
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
