aafb477b5774648095dee8685d5b079d8d11e212
Laertis Vaso <laertis.vaso@gmail.com>
2025-03-02 19:18:23 +0100

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.reduce( (oldest, person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        };
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        };
+        if (oldest.yearOfDeath - oldest.yearOfBirth < person.yearOfDeath - person.yearOfBirth) {
+            return person;
+        };
+        return oldest;
+     }, people[0]);
+     return oldest;
 };
 
 // Do not edit below this line
