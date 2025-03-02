111c0261cdf3cc37d7d745aa8513db6dc20dd522
Laertis Vaso <laertis.vaso@gmail.com>
2025-03-02 19:02:01 +0100

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    num = parseInt(num);
+    let numFibbonacci = { 0: 0, 1: 1 };
+    if (numFibbonacci[num]) {
+        return numFibbonacci[num];
+    } else {
+        for (let i = 2; i <= num; i++) {
+            numFibbonacci[i] = numFibbonacci[i - 1] + numFibbonacci[i - 2];
+        };
+        return numFibbonacci[num];
+    }; 
 };
 
 // Do not edit below this line
