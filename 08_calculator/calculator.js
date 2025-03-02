cc5d3034c451ca78fe224f3a31f38fad2b29e337
Laertis Vaso <laertis.vaso@gmail.com>
2025-03-02 18:48:11 +0100

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) return 0;
+  return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) { 
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i);
+  };
+  return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
 // Do not edit below this line
