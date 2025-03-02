5d8e3a031c2615d79d29f9571ddede9ad8c6015c
Laertis Vaso <laertis.vaso@gmail.com>
2025-03-02 18:55:31 +0100

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const cleanText = text.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    const reversedText = cleanText.split('').reverse().join('');
+    return cleanText === reversedText;
 };
 
 // Do not edit below this line
